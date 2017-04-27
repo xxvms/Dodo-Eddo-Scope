f8ae25509d50881dcce123209b8f814a77993f71
Tom Breza <tom@pcservicegroup.co.uk>
2017-04-27 19:55:57 +0100

    Add project files.

---
+++ Dodo-Eddo-Scope/Dodo-Eddo-Scope.cpp
@@ -0,0 +1,28 @@
+// Dodo-Eddo-Scope.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+void dodo();
+void eddo();
+
+
+int main()
+{
+	dodo();
+	eddo();
+
+	system("pause");
+    return 0;
+}
+void dodo()
+{
+	int i = 1;
+	std::cout << i << std::endl;
+}
+void eddo()
+{
+	int j = 2;
+	std::cout << j << std::endl;
+}
+
